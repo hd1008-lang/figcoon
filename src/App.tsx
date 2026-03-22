851190a0713ce3711c2b60cfe4c70f542d13610e
hd1009-lang <hoangdai.cnpm@gmail.com>
2026-03-22 09:53:10 +0700

    feat: react-version

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
+import { Figcoon } from "@/widgets/figcoon/ui/Figcoon";
+
 function App() {
-  const handleCreate = () => {
-    const count = 5;
-    parent.postMessage(
-      { pluginMessage: { type: "create-shapes", count } },
-      "*",
-    );
-    // Implementation for creating rectangles
-  };
+  // const handleCreate = () => {
+  //   const count = 5;
+  //   parent.postMessage(
+  //     { pluginMessage: { type: "create-shapes", count } },
+  //     "*",
+  //   );
+  //   // Implementation for creating rectangles
+  // };
 
   return (
-    <div className="">
-      <h2 className="text-amber-200">Rectangle Creator</h2>
-      <p className="text-red-400">
-        Count: <input id="count" type="number" value="5" />
-      </p>
-      <button id="create" onClick={handleCreate}>Create2</button>
-    </div>
+    <div className="size-150"><Figcoon /></div>
   );
 }
 
